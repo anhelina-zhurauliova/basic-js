dbde817ac102d74926290665368ab48f4700111e
Zhurauliova Anhelina <anhelinazhurauliova@gmail.com>
2020-03-02 23:25:37 +0300

    feat: solve count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let cats = 0;
+  let matrix2 = matrix.flat(Infinity);
+  for (let i = 0; i < matrix2.length; i++) {
+    if (matrix2[i] === "^^") {
+      cats++;
+    }
+  }
+  return cats;
 };
