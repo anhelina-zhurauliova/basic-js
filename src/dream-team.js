90fd64c3e62f252c8f0820d0c66df90517a17853
Zhurauliova Anhelina <anhelinazhurauliova@gmail.com>
2020-03-04 00:07:31 +0300

    feat: resolve task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,27 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let final = [];
+  if (
+    !members ||
+    members.length === undefined ||
+    typeof members === "number" ||
+    typeof members === "boolean"
+  )
+    return false;
+  else {
+    members.sort();
+    let filtered = members.filter(member => typeof member === "string");
+    let final = [];
+    for (let i = 0; i < filtered.length; i++) {
+      final.push(
+        filtered[i]
+          .trim()
+          .toUpperCase()
+          .slice(0, 1)
+      );
+    }
+    return final
+      .sort()
+      .toString()
+      .replace(/,/g, "");
+  }
 };
